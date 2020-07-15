e02084b40bfd298252b272efa1892c75af67437d
Justin Lowen <37968405+JLowe-N@users.noreply.github.com>
2020-07-15 15:11:40 -0500

    Revert "Master"

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from portfolioapp.views import IndexView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view())
 ]
