6dd4807bd658a2ada72142b2502edb4c4f79faa0
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-07-17 18:15:33 -0500

    Adding staticfiles

---
+++ staticfiles/portfolioapp/js/index.6f083aa8c6dd.js
@@ -0,0 +1,38 @@
+function getRandomColor() {
+    return Math.floor(Math.random()*16777215).toString(16)
+}
+
+function updateBGColor() {
+    let colorHex = getRandomColor()
+    document.getElementsByTagName("main")[0].style.backgroundColor = `#${colorHex}`
+}
+
+function updateBgColor() {
+    let colorHex = getRandomColor()
+    document.getElementsByTagName("main")[0].style.backgroundColor = `#${colorHex}`
+}
+
+function updateTextColor() {
+    let colorHex = getRandomColor()
+    document.getElementsByTagName("main")[0].style.color = `#${colorHex}`
+}
+
+function loopColors() {
+    for(let i = 0; i<100; i++) {
+        setTimeout(function() {
+                        updateBGColor()
+                        updateTextColor()
+                    }, 1000 * i            // i factor is needed, because all loops start their time together.
+        )
+    }
+}
+
+let bodyColorButton = document.getElementById("bgColorChange")
+bodyColorButton.addEventListener("click", updateBgColor)
+
+let textColorButton = document.getElementById("textColorChange")
+textColorButton.addEventListener("click", updateTextColor)
+
+let surpriseMeButton = document.getElementById("surpriseMe")
+surpriseMeButton.addEventListener("click", loopColors)
+
