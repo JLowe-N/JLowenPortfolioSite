132b1a54aa3d18b3c7944756cc23d1e8008558a7
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-04 18:53:59 -0500

    Add event listeners for getting joke and punchline

--- portfolioapp/static/portfolioapp/js/jokepage.js
+++ portfolioapp/static/portfolioapp/js/jokepage.js
@@ -1,16 +1,26 @@
-const setupDiv = document.getElementById("setup")
-const punchlineDiv = document.getElementById("punchline")
-const punchlineBtn = document.getElementById("punchlineBtn")
-const newJokesBtn = document.getElementById("newJokesBtn")
+const setupDiv = document.getElementById("setup");
+const punchlineDiv = document.getElementById("punchline");
+const punchlineBtn = document.getElementById("punchlineBtn");
+const newJokesBtn = document.getElementById("newJokesBtn");
+let punchline;
 
-async function getJoke() {
-    const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random')
-    const joke = await jokePromise.json()
-
-    setupDiv.innerHTML = joke[0].setup
-    punchlineDiv.innerHTML = joke[0].punchline
-    console.log(joke)
+function getPunchline() {
+    punchlineDiv.classList.add("bubble")
+    punchlineDiv.innerHTML = punchline;
 
 }
 
-getJoke()
+punchlineBtn.addEventListener("click", getPunchline)
+
+async function getJoke() {
+    const jokePromise = await fetch("https://official-joke-api.appspot.com/jokes/programming/random");
+    const joke = await jokePromise.json();
+
+    setupDiv.innerHTML = joke[0].setup;
+    punchline = joke[0].punchline;
+    console.log(joke);
+
+}
+
+getJoke();
+newJokesBtn.addEventListener('click', getJoke);
