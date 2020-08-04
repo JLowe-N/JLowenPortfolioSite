38f160c820a4fd55bf785d66abea3e8c02bce86a
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-04 17:57:18 -0500

    Joke API and JSON values set up

--- portfolioapp/static/portfolioapp/js/jokepage.js
+++ portfolioapp/static/portfolioapp/js/jokepage.js
@@ -7,6 +7,8 @@ async function getJoke() {
     const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random')
     const joke = await jokePromise.json()
 
+    setupDiv.innerHTML = joke[0].setup
+    punchlineDiv.innerHTML = joke[0].punchline
     console.log(joke)
 
 }
