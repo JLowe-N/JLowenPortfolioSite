b818eab1f1a6755513e3c465923f7f4e523a06c3
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-04 19:10:29 -0500

    Joke cycle complete

--- portfolioapp/static/portfolioapp/js/jokepage.js
+++ portfolioapp/static/portfolioapp/js/jokepage.js
@@ -1,28 +1,38 @@
 const setupDiv = document.getElementById("setup");
 const punchlineDiv = document.getElementById("punchline");
 const punchlineBtn = document.getElementById("punchlineBtn");
-const newJokesBtn = document.getElementById("newJokesBtn");
+const newJokeBtn = document.getElementById("newJokeBtn");
 let punchline;
 
 function getPunchline() {
     punchlineDiv.classList.add("bubble");
     punchlineDiv.innerHTML = punchline;
     punchlineBtn.classList.toggle("hidden");
-    newJokesBtn.classList.toggle("hidden")
+    newJokeBtn.classList.toggle("hidden");
 
 }
 
 punchlineBtn.addEventListener("click", getPunchline)
 
 async function getJoke() {
+    // Fetch the joke from the API, ensure JS waits for fetch to complete
     const jokePromise = await fetch("https://official-joke-api.appspot.com/jokes/programming/random");
     const joke = await jokePromise.json();
 
+    // Display the setup immediately
     setupDiv.innerHTML = joke[0].setup;
-    punchline = joke[0].punchline;
-    console.log(joke);
 
+    // Store the punchline for later
+    punchline = joke[0].punchline;
+
+    // Reset the punchline to initial state
+    punchlineDiv.innerHTML = '';
+    punchlineDiv.classList.remove("bubble")
+
+    // Toggle the button visibility
+    punchlineBtn.classList.toggle("hidden")
+    newJokeBtn.classList.toggle("hidden")
 }
 
 getJoke();
-newJokesBtn.addEventListener('click', getJoke);
+newJokeBtn.addEventListener('click', getJoke);
