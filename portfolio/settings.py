2f36633f0d966f7e2b0908ede10c84865fa45fa5
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-17 07:56:59 -0500

    Add staticfile_dir setting for pic some

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,6 +142,9 @@ REACT_APP_DIR = os.path.join(
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'static')
+]
 
 # May need to add additional variables for static to work on Heroku
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
